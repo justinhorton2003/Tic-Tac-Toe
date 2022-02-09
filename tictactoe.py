ef4a3ee429874936d163b1de4514f2af664e2a3e
Justin Horton <36089711+justinhorton2003@users.noreply.github.com>
2022-02-09 14:29:22 +0100

    Notable bugs sorted
    Now always asks for new input after space is taken or invalid character or number out of range.

--- tictactoe.py
+++ tictactoe.py
@@ -2,60 +2,141 @@ def play():#Board
     game = [[0,0,0],
             [0,0,0],
             [0,0,0]]
+
+    #Print Empty Board
     print("   0  1  2")
     count = 0        
     for row in game:
         print(count,row)    
         count = count+1
-    turn = 0
-    while (turn < 9):
+    turn = 0 #Turn Number at Start
+
+    while (turn < 9): #Alternating Turns
         #Player 1:
         if (turn % 2) == 0:
             print("Player 1's Turn:")
-            a = int(input(" Enter Row:"))
-            b = int(input(" Enter Column:"))
-            while game[a][b] != 0:
-                print("-Space is Taken-")
-                a = int(input(" Retry Row:"))
-                b = int(input(" Retry Column:"))
-            if game[a][b] == 0:
+
+            while True: #Non Number Input
+                try:
+                    a = int(input(" Enter Row:"))
+                    b = int(input(" Enter Column:"))
+                    break #Breaks out of loop if ValueError is (a,b>2)
+                except ValueError:
+                    print(" --Input must be 0,1 or 2--")
+                    continue
+
+            while a>2 or b>2: #Input Out of Range
+                try:
+                    print(" --Input must be 0,1 or 2--")
+                    a = int(input(" Retry Row:"))
+                    b = int(input(" Retry Column:"))
+                except ValueError:
+                    print(" --Input must be 0,1 or 2--")
+                    continue
+                
+            while game[a][b] != 0: #If Space is Occupied
+                try:
+                    print(" --Space is Taken--")
+                    a = int(input(" Retry Row:"))
+                    b = int(input(" Retry Column:"))
+                    while a>2 or b>2: #If After Space Taken an invalid input is given (a,b>2) it asks user again
+                        
+                        try:
+                            print(" --Input must be 0,1 or 2--")
+                            a = int(input(" Retry Row:"))
+                            b = int(input(" Retry Column:"))
+                        except ValueError: #After giving invalid input (a,b>2). Giving another (a,b>2) or (letter..) asks user again
+                            print(" --Input must be 0,1 or 2--")
+                            continue
+
+                except ValueError: #If After Space Taken a non-number is given asks user again
+                    print(" --Input must be 0,1 or 2--")
+                    continue
+                
+            if game[a][b] == 0: #Player 1 Places
                 game[a][b] = 1
         #Player 2
         else:
             print("Player 2's Turn:")
-            a = int(input(" Enter Row:"))
-            b = int(input(" Enter Column:"))
-            while game[a][b] != 0:
-                print("-Space is Taken-")
-                a = int(input(" Retry Row:"))
-                b = int(input(" Retry Column:"))
-            if game[a][b] == 0:
+
+            while True: #Non Number Input
+                try:
+                    a = int(input(" Enter Row:"))
+                    b = int(input(" Enter Column:"))
+                    break
+                except ValueError:
+                    print(" --Input must be 0,1 or 2--")
+                    continue
+
+            while a>2 or b>2: 
+                try:
+                    print(" --Input must be 0,1 or 2--")
+                    a = int(input(" Retry Row:"))
+                    b = int(input(" Retry Column:"))
+                except:
+                    print(" --Input must be 0,1 or 2--")
+                    continue
+
+            while game[a][b] != 0: #If Space is Occupied
+                try:
+                    print(" --Space is Taken--")
+                    a = int(input(" Retry Row:"))
+                    b = int(input(" Retry Column:"))
+                    while a>2 or b>2: #If After Space Taken an invalid input is given (a,b>2) it asks user again
+
+                        try:
+                            print(" --Input must be 0,1 or 2--")
+                            a = int(input(" Retry Row:"))
+                            b = int(input(" Retry Column:"))
+                        except ValueError: #After giving invalid input (a,b>2). Giving another (a,b>2) or (letter..) asks user again
+                            print(" --Input must be 0,1 or 2--")
+                            continue
+
+                except:
+                    print(" --Input must be 0,1 or 2--")
+                    continue
+
+            if game[a][b] == 0: #Player 2 Places
                 game[a][b] = 2
-        print("   0  1  2")#Board Numbering
+
+        #Print Board After Each Turn       
+        print("   0  1  2")
         count = 0
         for row in game:
             print(count, row)
             count = count + 1
-        turn = turn+1
-        def win():#Horizontal,Vertical,Diagonal Conditions
-            if game[a][0] == 1 and game[a][1] == 1 and game[a][2] == 1\
-            or game[0][b] == 1 and game[1][b] == 1 and game[2][b] == 1\
-            or game[1][1] == 1 and game[0][0] == 1 and game[2][2] == 1\
-            or game[1][1] == 1 and game[2][0] == 1 and game[0][2] == 1:
-                print("Player 1 Wins");return 1
-            if game[a][0] == 2 and game[a][1] == 2 and game[a][2] == 2\
-            or game[0][b] == 2 and game[1][b] == 2 and game[2][b] == 2\
-            or game[1][1] == 2 and game[0][0] == 2 and game[2][2] == 2\
-            or game[1][1] == 2 and game[2][0] == 2 and game[0][2] == 2:
-                print("Player 2 Wins");return 1
-        if win()==1:
+        turn = turn+1 #Turn Number After Each Turn
+
+        def win(p):#Horizontal,Vertical,Diagonal Win Conditions
+            if game[a][0] == p and game[a][1] == p and game[a][2] == p\
+            or game[0][b] == p and game[1][b] == p and game[2][b] == p\
+            or game[1][1] == p and game[0][0] == p and game[2][2] == p\
+            or game[1][1] == p and game[2][0] == p and game[0][2] == p:
+                return 1
+
+        if win(p=1)==1: #Win
+            print(" --Player 1 Wins--")
             turn = turn+(9-turn) #Reset Board
-while True:
-    x = int(input("""
-Enter 1 for New Game 
-Enter 2 for Exit"""))
-    if x==1:
-        play()
-    if x==2:
-        break
+        if win(p=2)==1:
+            print(" --Player 2 Wins--")
+            turn = turn+(9-turn) #Reset Board
+
+def structure(): #Structure of Events
+    while True:
+        try:
+            x = input(
+"""Enter 1 for New Game 
+Enter 2 for Exit""")
+            if int(x)==1:
+                play()
+            if int(x)==2:
+                break
+        except:
+            print(" --Input must be 0,1 or 2--")
+print("v1") #Version Testing 
+
+structure() #The Program
+
+"""All Input Errors fixed, shorten code"""
+
 
